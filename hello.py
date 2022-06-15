02d0c06f8a65ecacb9ca12f54db330fe2d1a6a4a
usaurabh02 <saurabhu154@example.com>
2022-06-15 19:37:43 +0530

    Auto deploy test

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hi!! Saurabh. Flask inside Docker is up and running twice!!"
+    return "Hi!! Saurabh. Flask inside Docker is up and running thrice!!"
 
 
 if __name__ == "__main__":
