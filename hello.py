774f0daf3e86fe6c4b146ded6102ae8e6b5c6a57
usaurabh02 <saurabhu154@example.com>
2022-06-16 13:30:21 +0530

    Update

--- hello.py
+++ hello.py
@@ -5,12 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-<<<<<<< HEAD
     return "Hi!! Saurabh. Flask inside Docker is up and running for final testing working"
 
-=======
-    return "Hi!! Saurabh. Flask inside Docker is up and running for final test11!!"
->>>>>>> 3a8f032fed1b3009aabc439706924307b9322614
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
